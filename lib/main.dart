eb881f9c11c3ee8ab78b7c8f781fce13a3f02084
Mumbere7 <arnoldnzibuk@gmmail.com>
2020-06-23 15:43:48 +0300

    Fixing main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:marcheappadmi/providers/app_state.dart';
+import 'package:marcheappadmi/providers/product_provider.dart';
 import 'package:marcheappadmi/screens/admin.dart';
+import 'package:marcheappadmi/screens/dashboard.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+ main(){
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider.value(value: AppState()),
+      ChangeNotifierProvider.value(value: ProductProvider()),
+
+    ],
+    child: MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.deepOrange.shade900,
-      ),
-      home: AdminPanel(),
+      home: Dashboard(),
     ),
-  );
+  ));
 }
 
